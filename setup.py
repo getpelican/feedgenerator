15b3e886034ed65e8bbbf2b76bb771ae9ae9d1eb
Dirk Makowski <dmakowski@gmx.net>
2012-08-24 00:43:16 +0200

    Added original sources to start porting

---
+++ setup.py
@@ -0,0 +1,43 @@
+#! /usr/bin/python
+from distutils.core import setup
+
+
+NAME = 'feedgenerator'
+MODULES = ['feedgenerator']
+DESCRIPTION = 'Standalone version of django.utils.feedgenerator'
+
+URL = "http://hg.lolnet.org/feedgenerator/"
+
+CLASSIFIERS = ['Development Status :: 5 - Production/Stable',
+               'Environment :: Web Environment',
+               'Intended Audience :: Developers',
+               'License :: OSI Approved :: BSD License',
+               'Operating System :: OS Independent',
+               'Programming Language :: Python',
+               'Topic :: Internet :: WWW/HTTP',
+               'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+               'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+AUTHOR = 'Django Software Foundation'
+AUTHOR_EMAIL = 'foundation@djangoproject.com'
+MAINTAINER = 'Alexis Metaireau'
+MAINTAINER_EMAIL= 'alexis@notmyidea.org'
+KEYWORDS = "feed atom rss".split(' ')
+VERSION = '1.2.1'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    py_modules=MODULES,
+    
+    # metadata for upload to PyPI
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    description=DESCRIPTION,
+    keywords=KEYWORDS,
+    url=URL,
+    classifiers=CLASSIFIERS,
+)
