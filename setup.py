232fb7aafcde2c957f42d1d0adb0cbda13aeb495
Alexis Métaireau <alexis@notmyidea.org>
2013-08-31 19:55:16 +0200

    bump version

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ AUTHOR_EMAIL = 'foundation@djangoproject.com'
 MAINTAINER = 'Dirk Makowski'
 MAINTAINER_EMAIL = 'dm@parenchym.com'
 KEYWORDS = "feed atom rss".split(' ')
-VERSION = '1.6'
+VERSION = '1.7'
 
 TEST_SUITE = 'tests_feedgenerator'
 
