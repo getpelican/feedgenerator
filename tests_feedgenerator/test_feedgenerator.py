ecfb8f7221828339ef36c65ab76e1ddc143871c3
Alex Brown <alex@alexanderdbrown.com>
2014-01-06 11:12:06 +0000

    Remove unnecessary atom elements.

--- tests_feedgenerator/test_feedgenerator.py
+++ tests_feedgenerator/test_feedgenerator.py
@@ -29,7 +29,7 @@ FIXT_ITEM = dict(
 
 
 EXPECTED_RESULT = """<?xml version="1.0" encoding="utf-8"?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom"><channel><title>Poynter E-Media Tidbits</title><link>http://www.poynter.org/column.asp?id=31</link><description>A group Weblog by the sharpest minds in online media/journalism/publishing.
+<rss version="2.0"><channel><title>Poynter E-Media Tidbits</title><link>http://www.poynter.org/column.asp?id=31</link><description>A group Weblog by the sharpest minds in online media/journalism/publishing.
     Umlauts: äöüßÄÖÜ
     Chinese: 老师是四十四，是不是？
     Finnish: Mustan kissan paksut posket. (ah, no special chars) Kärpänen sanoi kärpäselle: tuu kattoon kattoon ku kaveri tapettiin tapettiin.
